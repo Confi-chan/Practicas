689b056fcbf2d44c8fa8f3757e650415474b056a
Confi-chan <sofia.lopez@alumnos.uacm.edu.mx>
2025-11-11 23:21:25 -0600

    confirmo

--- Practica4/Practica 4/main.c
+++ Practica4/Practica 4/main.c
@@ -19,3 +19,9 @@ typedef struct {
     int idTarea;
     float calificacion;
 } Calificacion;
+
+// Variables globales
+Tarea tareas[MAX_TAREAS];
+Alumno alumnos[MAX_ALUMNOS];
+Calificacion calificaciones[MAX_ALUMNOS * MAX_TAREAS];
+int contadorTareas = 0, contadorAlumnos = 0, contadorCalificaciones = 0;
