ff56e6969fea7a78beac88d7e6a099081bd0828a
Confi-chan <sofia.lopez@alumnos.uacm.edu.mx>
2025-11-11 23:18:53 -0600

    cofirmo

--- Practica4/Practica 4/main.c
+++ Practica4/Practica 4/main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
-}
+#define MAX_ALUMNOS 50
+#define MAX_TAREAS 50
+
+// Estructuras
+typedef struct {
+    char nombre[50];
+    char descripcion[100];
+} Tarea;
+
+typedef struct {
+    char nombre[50];
+} Alumno;
+
+typedef struct {
+    int idAlumno;
+    int idTarea;
+    float calificacion;
+} Calificacion;
