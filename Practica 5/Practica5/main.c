4c4d195e30623f5f0e7545ac768aa09c49dcbec9
Confi-chan <sofia.lopez@alumnos.uacm.edu.mx>
2025-11-13 12:13:03 -0600

    confirmo

--- Practica 5/Practica5/main.c
+++ Practica 5/Practica5/main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
-}
+#define MAX_ALUMNOS 50
+#define MAX_TAREAS 50
+#define MAX_PERSONAS 50
+
+// Estructuras principales
+
+// Estructura para guardar los datos de una tarea
+typedef struct {
+    char nombre[50];
+    char descripcion[100];
+} Tarea;
