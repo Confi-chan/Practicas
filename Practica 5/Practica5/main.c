52e16a831a09ae03f793af750595ff5fe8626c2e
Confi-chan <sofia.lopez@alumnos.uacm.edu.mx>
2025-11-13 12:17:07 -0600

    confirmo

--- Practica 5/Practica5/main.c
+++ Practica 5/Practica5/main.c
@@ -9,7 +9,45 @@
 // Estructuras principales
 
 // Estructura para guardar los datos de una tarea
-typedef struct {
+typedef struct
+{
     char nombre[50];
     char descripcion[100];
 } Tarea;
+
+// Estructura para guardar los datos de un alumno
+typedef struct
+{
+    char nombre[50];
+} Alumno;
+
+// Estructura que relaciona un alumno con una tarea y su calificación
+typedef struct
+{
+    int idAlumno;
+    int idTarea;
+    float calificacion;
+} Calificacion;
+
+// Estructura para el editor de texto (nueva parte)
+typedef struct
+{
+    char nombre[50];
+    char cargo[50];
+} Persona;
+
+
+// Variables globales
+// ----------------------------
+Tarea tareas[MAX_TAREAS];
+Alumno alumnos[MAX_ALUMNOS];
+Calificacion calificaciones[MAX_ALUMNOS * MAX_TAREAS];
+
+int contadorTareas = 0;
+int contadorAlumnos = 0;
+int contadorCalificaciones = 0;
+
+// Variables para el módulo de texto
+Persona personas[MAX_PERSONAS];
+int contadorPersonas = 0;
+char *mensaje = NULL; // Texto base (memoria dinámica)
